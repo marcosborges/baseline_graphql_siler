3c2636cfeaedfeb4220b38a45768b5eabcf1e1ba
Marcos Borges <contato@marcosborges.com>
2020-04-30 21:37:56 -0300

    Ajuste na rota para o endpoint health check

--- server.php
+++ server.php
@@ -2,21 +2,16 @@
 <?php declare(strict_types=1);
 
 use Siler\Route;
-use function Siler\Swoole\{graphql_handler, http, json, not_found};
+use Siler\Functional as λ;
+use function Siler\Swoole\{graphql_handler, http, json};
 
 require_once __DIR__ . '/bootstrap.php';
+global $schema, $root_value, $context;
 
-$health = fn() => json(['status' => 'ok']);
-$graphql = graphql_handler($schema, $root_value, $context);
-
-$handler = function () use ($health, $graphql): void {
-    Route\get('/health', $health);
-    Route\post('/graphql', $graphql);
-
-    if (!Route\did_match()) {
-        not_found();
-    }
+$handler = graphql_handler($schema, $root_value, $context);
+$server = function () use ($handler): void {
+    Route\get('/health', json(['status' => 'ok']));
+    Route\post('/graphql', $handler);
 };
 
-$server = http($handler, 9501);
-$server->start();
+http($server, 9501)->start();
