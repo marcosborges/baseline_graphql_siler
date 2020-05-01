13b05a2cde4b94d9a29aad270d3c794100aed9ac
Marcos Borges <contato@marcosborges.com>
2020-04-30 22:02:30 -0300

    Ajuste endpoint health

--- server.php
+++ server.php
@@ -3,15 +3,16 @@
 
 use Siler\Route;
 use Siler\Functional as λ;
-use function Siler\Swoole\{graphql_handler, http, json};
+use function Siler\Swoole\{graphql_handler, http, json, emit};
 
 require_once __DIR__ . '/bootstrap.php';
 global $schema, $root_value, $context;
 
 $handler = graphql_handler($schema, $root_value, $context);
 $server = function () use ($handler): void {
-    Route\get('/health', json(['status' => 'ok']));
     Route\post('/graphql', $handler);
+    Route\get('/health', fn() => json(['status' => 'ok']));
+    emit('Not found', 404);
 };
 
 http($server, 9501)->start();
