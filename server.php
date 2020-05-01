a4fbcb994394fdc1a31dee3b84231f2ca2103133
leocavalcante <lc@leocavalcante.com>
2020-04-30 22:07:03 -0300

    Fix and improve routing

--- server.php
+++ server.php
@@ -4,25 +4,14 @@
 use Siler\Route;
 use Siler\Functional as λ;
 use function Siler\Swoole\{graphql_handler, http, json};
-use function Siler\Swoole\{graphql_handler, http, json, not_found};
 
 require_once __DIR__ . '/bootstrap.php';
-
-$health = fn() => json(['status' => 'ok']);
-$graphql = graphql_handler($schema, $root_value, $context);
+global $schema, $root_value, $context;
 
 $handler = graphql_handler($schema, $root_value, $context);
 $server = function () use ($handler): void {
     Route\get('/health', json(['status' => 'ok']));
     Route\post('/graphql', $handler);
-$handler = function () use ($health, $graphql): void {
-    Route\get('/health', $health);
-    Route\post('/graphql', $graphql);
-
-    if (!Route\did_match()) {
-        not_found();
-    }
 };
 
-$server = http($handler, 9501);
-$server->start();
+http($server, 9501)->start();
